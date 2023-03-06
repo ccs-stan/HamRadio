5b03f8f9d10e4172aa0b603e554a5ccfb68faa8f
Stan Crump <103955156+ccs-stan@users.noreply.github.com>
2023-03-05 19:30:21 -0600

    Create index.php
    Initial, untested, first cut.

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            .info { display: none; height: 42px; }
+            body {background-color:#008ddf; color:white; font-size: 1.5em; text-shadow: black 1px 1px;}
+            footer {font-size: smaller;}
+        </style>
+    </head>
+    <body>
+        <h1>Ham Radio Info for Newbies</h1>
+        <h2 class='header' id="baofengkeys">Baofeng Radio Keys</h2>
+        <div class='info' id="baofengkeys" onclick="hideUp()">
+            <p>
+            This is a quick summary of the key usage for the Baofeng radio.
+            <ul>
+                    <li>Exit - Allows you to exit the menu you have selected or are currently viewing. </li>
+                    <li>Step - Allows you to access your frequency steps (2.5/5/6.25/10/12.5/20/25/50kHz)</li>
+                    <li>TXP - Allows you to switch and set transmission power (High/Medium/Low)</li>
+                    <li>Save - Allows you to access your Battery Saver settings (OFF/1/2/3/4)</li>
+                    <li>*Scan - Allows you to swap through TX/RX frequencies, scan and set RX CTCCS/DCS tones.</li>
+                    <li>VOX - Allows you to set your voice operated transmission settings (OFF/0 - 10)</li>
+                    <li>W N - Allows you to switch between Wideband and Narrowband settings (WIDE/NARR)</li>
+                    <li>ABR - Allows you to adjust the display illumination time length. (Off/1/2/3/4/5 seconds)</li>
+                    <li>SQL - Allows you to access the squelch settings and adjust squelch levels (0 - 9)</li>
+                    <li>TDR - Allows you to switch between dual watch and dual reception. (Off/On)</li>
+                    <li>BEEP - Allows you to adjust the keypad's beep sound. (Off/On)</li>
+                    <li>TOT - Allows you to access your transmission timer settings. (15/30/45/60.../585/600 seconds.</li>
+            </ul>
+        </p>
+        </div>
+        <h2 class='header' id="repeaters"">Power Down</h2>
+        <div class='info' id="repeaters">
+            <p>
+            Local repeaters - DeSoto County, Mississippi
+              <ul>
+                <li>145.2700 	-0.6 MHz 	107.2 	Horn Lake 	DeSoto 	N5NBG 	OPEN 	FM 	ON-AIR</li>
+                <li>145.3500 	-0.6 MHz 	107.2 / 107.2 	Southaven 	DeSoto 	W5GWD 	OPEN 	FM 	ON-AIR</li>
+                <li>145.3700 	-0.6 MHz 	107.2 / 107.2 	Hernando 	DeSoto 	N5PYQ 	OPEN 	FM 	ON-AIR</li>
+                <li>145.5500 	0 MHz 		Horn Lake 	DeSoto 	W5AV 	OPEN 	DSTAR 	ON-AIR</li>
+                <li>146.9100 	-0.6 MHz 	107.2 / 107.2 	Hernando 	DeSoto 	W5GWD 	OPEN 	FM 	ON-AIR</li>
+                <li>147.2550 	+0.6 MHz 	79.7 	Olive Branch, Water tower Hwy 78 & Craft Road 	DeSoto 	W5OBM 	OPEN 	FM EchoLink 	ON-AIR</li>
+                <li>442.0125 	+5 MHz 	CC1 	Horn Lake 	DeSoto 	W5AV 	OPEN 	DMR 	ON-AIR</li>
+                <li>442.8000 	+5 MHz 	100.0 / 100.0 	Olive Branch 	DeSoto 	KD5BS 	OPEN 	FM Fusion 	Testing</li>
+                <li>444.5500 	+5 MHz 	100.0 / 100.0 	Olive Branch 	DeSoto 	KD5BS 	OPEN 	FM Fusion WIRES-X 	ON-AIR</li>
+                <li>444.7000 	+5 MHz 	107.2 / 107.2 	Olive Branch, Lewisburg Water Tower 	DeSoto 	W5OBM 	OPEN 	FM 	ON-AIR</li>
+              </ul>
+          </p>
+        </div>
+        <h2 class='header' id="freq">Playing a CD</h2>
+        <div class='info' id="freq">
+        <p>
+            Amateur Techician Privileges (2012 - ARRL) (1500 Watts PEP Max except for 28.300-28.500MHz)
+            <ul>
+                <li>1240-1300MHz - SSB, AM, FM, TV</li>
+                <li>902-928MHz - SSB, AM, FM, TV</li>
+                <li>430-450MHz - SSB, AM, FM, TV</li>
+                <li>222-225MHz - SSB, AM, FM, TV</li>
+                <li>144.1-148MHz - SSB, AM, FM, TV</li>
+                <li>50.1-54MHz - SSB, AM, FM, TV</li>
+                <li>21.025-21.200MHz - CW only</li>
+                <li>7.025-7.125MHz - CW only</li>
+                <li>3.525-3.600MHz - CW only</li>
+                <li>219-220MHz - Point to Point Digital Links only</li>
+                <li>28.300-28.500MHz - SSB & CW (200 Watts PEP Max)</li>
+            </ul>
+        </p>
+        </div>
+        <h2 class='header' id="codes">Pastor is preaching</h2>
+        <div class='info' id="codes">
+            <p>
+                Contact Basics - Q-Signals
+                <ul>
+                    <li>QTH - My location is ___. What is your location?</li>
+                    <li>QTR - My time is ___. What is the correct time?</li>
+                </ul>
+            </p>
+        </div>
+        <h2 class='header' id="phonetic">Powerpoint Operation</h2>
+        <div class='info' id="phonetic">
+            <p>
+                Phonetic Alphabet
+                <ul>
+                    <li>A - Alfa</li>
+                    <li>B - Bravo</li>
+                    <li>C - Charlie</li>
+                    <li>D - Delta</li>
+                    <li>E - Echo</li>
+                    <li>F - Foxtrot</li>
+                    <li>G - Golf</li>
+                    <li>H - Hotel</li>
+                    <li>I - India</li>
+                    <li>J - Juliet</li>
+                    <li>K - Kilo</li>
+                    <li>L - Lima</li>
+                    <li>M - Mike</li>
+                    <li>N - November</li>
+                    <li>O - Oscar</li>
+                    <li>P - Papa</li>
+                    <li>Q - Quebec</li>
+                    <li>R - Romeo</li>
+                    <li>S - Sierra</li>
+                    <li>T - Tango</li>
+                    <li>U - Uniform</li>
+                    <li>V - Victor</li>
+                    <li>W - Whiskey</li>
+                    <li>X - X-Ray</li>
+                    <li>Y - Yankee</li>
+                    <li>Z - Zulu</li>
+                </ul>
+            </p>
+        </div>
+        <footer>Click a heading to display the details<br>Click the heading or details to hide the details</footer>
+        <script>
+            var tasks = document.querySelectorAll('.header')
+            var details = document.querySelectorAll('.info')
+            for(let i=0; i<tasks.length; i++) {
+                console.log(`Running for loop - value = ${i}`)
+                tasks[i].addEventListener('click', function(){
+                    let state = details[i].style.display
+                    console.log(`upClick processing - state = ${state}`)
+                    if(state == 'inline')
+                        details[i].style.display = "none";
+                    else
+                        details[i].style.display = "inline";
+                    state = details[i].style.display
+                    console.log(`upClick processing - state = ${state}`)
+                })
+                details[i].addEventListener('click', function(){
+                        details[i].style.display = "none";
+                })
+            }
+        </script>
+
+    </body>
+</html>
